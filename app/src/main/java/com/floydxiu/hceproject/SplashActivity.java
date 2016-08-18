c565878c6db674f32b913818c74cb5c65f1d73a5
XIu-Chi Chang <s94040511s@gmail.com>
2016-08-18 14:09:39 +0800

    Update# SplashActivity.java and Add# SplashInitialCheck.java

--- app/src/main/java/com/floydxiu/hceproject/SplashActivity.java
+++ app/src/main/java/com/floydxiu/hceproject/SplashActivity.java
@@ -1,5 +1,6 @@
 package com.floydxiu.hceproject;
 
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,28 @@ public class SplashActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_splash);
+    }
+
+    private class SplashDisplayTimer extends AsyncTask<Integer, Void, Void>{
+        @Override
+        protected Void doInBackground(Integer... params) {
+            int timer = params[0].intValue();
+            try {
+                Thread.sleep(timer*1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void aVoid) {
+            super.onPostExecute(aVoid);
+            //Direct App into true user interactive interface
+
+            //if first time use this app
+
+            //if user have login
+        }
     }
 }
---
+++ app/src/main/java/com/floydxiu/hceproject/SplashInitialCheck.java
@@ -0,0 +1,10 @@
+package com.floydxiu.hceproject;
+
+/**
+ * Created by Floyd on 2016/8/18.
+ * Use this Class to check the Initial Status of App
+ * Whether it is "First Time Open" or "Not Login" or "Login"
+ */
+
+public class SplashInitialCheck {
+}
