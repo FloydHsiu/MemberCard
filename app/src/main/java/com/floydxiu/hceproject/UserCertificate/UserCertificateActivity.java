e084e9e92d56c2bfa2fcaa49fc88574ab59fa316
XIu-Chi Chang <s94040511s@gmail.com>
2016-09-09 10:48:02 +0800

    Update# UserCertificate fragment transaction

--- app/src/main/java/com/floydxiu/hceproject/UserCertificate/UserCertificateActivity.java
+++ app/src/main/java/com/floydxiu/hceproject/UserCertificate/UserCertificateActivity.java
@@ -1,9 +1,16 @@
 package com.floydxiu.hceproject.UserCertificate;
 
+import android.app.Fragment;
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 import com.floydxiu.hceproject.R;
+import com.floydxiu.hceproject.UserCertificate.UserCertificateFragment.LoginFragment;
+import com.floydxiu.hceproject.UserCertificate.UserCertificateFragment.SignUpFragment;
 
 /**
  * Created by Floyd on 2016/8/19.
@@ -12,9 +19,52 @@ import com.floydxiu.hceproject.R;
  */
 
 public class UserCertificateActivity extends AppCompatActivity {
+    LoginFragment loginFragment = new LoginFragment();
+    SignUpFragment signUpFragment = new SignUpFragment();
+    FragmentManager fragmentManager;
+    FragmentTransaction fragmentTransaction;
+
+    Button btnSignup, btnLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_usercertificate);
+
+        //Initialize fragment state
+        fragmentManager = getFragmentManager();
+        fragmentTransaction = fragmentManager.beginTransaction();
+        fragmentTransaction.add(R.id.layoutUserCertificateFragment, loginFragment, "LOGIN");
+        fragmentTransaction.commit();
+
+
+        //Button binding
+        btnSignup = (Button)findViewById(R.id.btnSignup);
+        btnLogin = (Button)findViewById(R.id.btnLogin);
+
+        btnSignup.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment tempfragment = fragmentManager.findFragmentByTag("SIGNUP");
+                if(tempfragment == null || !tempfragment.isVisible()) {
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.layoutUserCertificateFragment, signUpFragment, "SIGNUP");
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Fragment tempfragment = fragmentManager.findFragmentByTag("LOGIN");
+                if(tempfragment == null || !tempfragment.isVisible()) {
+                    fragmentTransaction = fragmentManager.beginTransaction();
+                    fragmentTransaction.replace(R.id.layoutUserCertificateFragment, loginFragment, "LOGIN");
+                    fragmentTransaction.commit();
+                }
+            }
+        });
+
     }
 }
--- app/src/main/java/com/floydxiu/hceproject/UserCertificate/UserCertificateFragment/LoginFragment.java
+++ app/src/main/java/com/floydxiu/hceproject/UserCertificate/UserCertificateFragment/LoginFragment.java
@@ -1,10 +1,22 @@
 package com.floydxiu.hceproject.UserCertificate.UserCertificateFragment;
 
 import android.app.Fragment;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.floydxiu.hceproject.R;
 
 /**
  * Created by Floyd on 2016/8/19.
  */
 
 public class LoginFragment extends Fragment {
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_login, container, false);
+    }
 }
