bc21506c6b60f494d787d025fe3d40765a050393
XIu-Chi Chang <s94040511s@gmail.com>
2016-09-11 21:53:05 +0800

    Update# CardAndUserInfo Toolbar function testing

--- app/src/main/java/com/floydxiu/hceproject/CardAndUserInfo/CardAndUserInfoActivity.java
+++ app/src/main/java/com/floydxiu/hceproject/CardAndUserInfo/CardAndUserInfoActivity.java
@@ -3,6 +3,10 @@ package com.floydxiu.hceproject.CardAndUserInfo;
 import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+
+import com.floydxiu.hceproject.R;
 
 /**
  * Created by Floyd on 2016/9/10.
@@ -11,8 +15,30 @@ import android.support.v7.app.AppCompatActivity;
  */
 
 public class CardAndUserInfoActivity extends AppCompatActivity {
+
+    Toolbar toolbarCardAndUserInfo;
+
     @Override
     public void onCreate(Bundle savedInstanceState, PersistableBundle persistentState) {
         super.onCreate(savedInstanceState, persistentState);
+        setContentView(R.layout.activity_cardanduserinfo);
+
+        //Toolbar setting
+        toolbarCardAndUserInfo = (Toolbar) findViewById(R.id.toolbarCardAndUserInfo);
+        toolbarCardAndUserInfo.setTitle(R.string.app_name);
+        toolbarCardAndUserInfo.inflateMenu(R.menu.menu_cardlist);
+        //bind action on the item in menu
+        toolbarCardAndUserInfo.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener(){
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                switch (item.getItemId()){
+                    case R.id.itemCardListAdd:
+                        //do something
+                        break;
+                }
+                return true;
+            }
+        });
+
     }
 }
